1afbfe965fbfde763f9055f1dc924f50fb8e4fcf
Prisca Roseline <prisca.ros@onja.org>
2020-10-15 07:48:36 +0300

    I updated input component..

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -28593,7 +28593,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52244" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52938" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
